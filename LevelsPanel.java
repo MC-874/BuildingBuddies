a0feb9e7c4e5536cab348d1d1007954f703fb096
Daniela <102562736+DanielaRL@users.noreply.github.com>
2022-04-10 21:21:30 -0500

    Add files via upload

---
+++ LevelsPanel.java
@@ -0,0 +1,7 @@
+public class LevelsPanel extends JFrame{
+
+    LevelsPanel(){
+
+    }
+    
+}
---
+++ StartPanel.java
@@ -0,0 +1,13 @@
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class StartPanel extends JPanel {
+    
+
+    StartPanel(){
+        //create start panel
+        JLabel titleLabel = new JLabel("Welcome to Building Buddies!");
+
+        add(titleLabel);
+    }
+}
