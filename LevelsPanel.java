fac03bc5c633331aca96ef98e8fa8ed7d966cddd
Daniela <102562736+DanielaRL@users.noreply.github.com>
2022-04-16 19:29:02 -0500

    Update LevelsPanel.java

--- LevelsPanel.java
+++ LevelsPanel.java
@@ -3,6 +3,24 @@ import javax.swing.JPanel;
 public class LevelsPanel extends JPanel{
 
     LevelsPanel(){
+        //set panel format( boxLayout)
+
+        
+        //line 1
+        //create & format score jpanel
+
+        //Create & format score jlabel
+
+        //create format scoretotal Label
+
+
+
+        //line 2
+        //create titleLabel JLabel w/set Horizontal alignement = jlabel.center
+
+
+        //line 3
+
 
     }
     
