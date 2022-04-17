2cc707c87695167e429f16f26bb287f3904c0708
DanielaRL <102562736+DanielaRL@users.noreply.github.com>
2022-04-16 19:48:38 -0500

    updates

--- LevelsPanel.java
+++ LevelsPanel.java
@@ -3,16 +3,13 @@ import javax.swing.JPanel;
 public class LevelsPanel extends JPanel{
 
     LevelsPanel(){
-<<<<<<< HEAD
         int score;
         String[] nounVariables;
         String[] verbVariables;
-        String[]subjectVariables;
+        String[] subjectVariables;
         
         //set panel format (BoxLayout)
-=======
         //set panel format( boxLayout)
->>>>>>> a2e1a5f58b3b0dad9f37b5b9e21491ee583e8c61
 
         
         //line 1
@@ -21,23 +18,19 @@ public class LevelsPanel extends JPanel{
         //Create & format score jlabel
 
         //create format scoretotal Label
-<<<<<<< HEAD
         
         //add scoreJLabel & scoretotalLabel to scoreJPanel
 
 
-=======
 
         //add scoreJLabel & scoretotalLabel to scoreJPanel
         
         
->>>>>>> a2e1a5f58b3b0dad9f37b5b9e21491ee583e8c61
 
         //line 2
         //create titleLabel JLabel w/set Horizontal alignement = jlabel.center
 
 
-<<<<<<< HEAD
         //line 3-5
         //create middlepanel, type JPanel
 
@@ -59,9 +52,7 @@ public class LevelsPanel extends JPanel{
         //create userInputTextfield
 
 
-=======
         //line 3
->>>>>>> a2e1a5f58b3b0dad9f37b5b9e21491ee583e8c61
 
 
     }
