6cb1ae966452a879e1ad1815964910180bf6c650
DanielaRL <102562736+DanielaRL@users.noreply.github.com>
2022-04-16 19:40:29 -0500

    add variables, update outline

--- LevelsPanel.java
+++ LevelsPanel.java
@@ -3,6 +3,51 @@ import javax.swing.JPanel;
 public class LevelsPanel extends JPanel{
 
     LevelsPanel(){
+        int score;
+        String[] nounVariables;
+        String[] verbVariables;
+        String[]subjectVariables;
+        
+        //set panel format (BoxLayout)
+
+        
+        //line 1
+        //create & format score jpanel
+
+        //Create & format score jlabel
+
+        //create format scoretotal Label
+        
+        //add scoreJLabel & scoretotalLabel to scoreJPanel
+
+
+
+        //line 2
+        //create titleLabel JLabel w/set Horizontal alignement = jlabel.center
+
+
+        //line 3-5
+        //create middlepanel, type JPanel
+
+        //create & format nounPanel
+
+        //for inside noun panel
+        //create &format nounLabel
+
+        //create & format nounTextfield
+
+        //add nounLabel and nounTextfield to nounPanel
+
+
+        //create userInputPanel
+
+        //inside userInputPanel:
+        //create instructionLabel
+
+        //create userInputTextfield
+
+
+
 
     }
     
