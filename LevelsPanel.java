4be8875d0849bf141b2a373ca5a47631937f7fd1
Max Clarke <80911882+MC-874@users.noreply.github.com>
2022-04-17 20:05:06 -0500

    Added layout for MiddlePanel
    Code for the panels in Middlepanel needs adjustment

--- LevelsPanel.java
+++ LevelsPanel.java
@@ -1,21 +1,36 @@
- MC-874-patch-1
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class LevelsPanel extends JPanel{
+    	JButton checkButton = new JButton("   Check your Sentence   ");
+    	JButton exitButton = new JButton("        Exit Game        ");
 
     LevelsPanel(){
-        int score;
-        String[] nounVariables = {"Cat", "Dog", "Toy", "Bear", "I", "You", "They", "We", "Bird", "Mouse"};
-        String[] verbVariables = {"ran", "ate", "went", "choose", "eats", "play"};
-        String[] objectVariables = {"far", "food", "home", "guitar", "outside", "school"};
+    	int score;
+    	String[] nounVariables = {"Cat", "Dog", "Toy", "Bear", "I", "You", "They", "We", "Bird", "Mouse"};
+    	String[] verbVariables = {"ran", "ate", "went", "choose", "eats", "play"};
+    	String[] objectVariables = {"far", "food", "home", "guitar", "outside", "school"};
         
-        //set panel format (BoxLayout)
+		//set panel format (BoxLayout)
         //set panel format( boxLayout)
 
         
         //line 1
         //create & format score jpanel
-
+        JPanel scorePanel = new JPanel();
+        scorePanel.setLayout(new BoxLayout(scorePanel, BoxLayout.Y_AXIS));
+        scorePanel.setAlignmentX(Component.LEFT_ALIGNMENT);
         //Create & format score jlabel
 
         //create format scoretotal Label
@@ -32,92 +47,103 @@ public class LevelsPanel extends JPanel{
         //create titleLabel JLabel w/set Horizontal alignement = jlabel.center
 
 
+      
         //line 3-5
         //create middlepanel, type JPanel
-
+        JPanel middlepanel = new JPanel();
+        
+        
         //create & format nounPanel
-
+        JPanel nounPanel = new JPanel();
         //for inside noun panel
+        setSize(400,50);
+        nounPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
+             
+        
         //create &format nounLabel
-
+        JLabel nounLabel = new JLabel("Nouns: ");
+        //nounLabel.setLayout(new BorderLayout());;
+               
         //create & format nounTextfield
-
+        JTextField nounTextField = new JTextField();
+        
+       
         //add nounLabel and nounTextfield to nounPanel
+        nounPanel.add(nounLabel, BorderLayout.EAST);
+        nounPanel.add(nounTextField, BorderLayout.WEST);
+        
+        
+        // Create Verb Panel
+        JPanel verbPanel = new JPanel();
+        
+        // Format verbPanel
+        //verbPanel =   
+        
+        
+        // Create SubjectPanel
+        JPanel subjectPanel = new JPanel();
+        
+        
+        middlepanel.add(nounPanel);
+        middlepanel.add(verbPanel);
+        middlepanel.add(subjectPanel);
 
-
+        
+        
         //create userInputPanel
-
+        JPanel userInputPanel= new JPanel();
+        userInputPanel.setLayout(new BoxLayout(userInputPanel , BoxLayout.Y_AXIS));
         //inside userInputPanel:
         //create instructionLabel
+        JLabel instructionLabel = new JLabel("Insert your Sentence:");
+        instructionLabel.setFont(new Font("titleFont", Font.BOLD, 24));
+        //create userInputTextField
+        JTextField userInputTextField = new JTextField("Type your sentences here (using the above word bank)",20);
+        userInputTextField.setEditable(true);
+        userInputTextField.setFont(new Font(Font.SANS_SERIF, Font.ROMAN_BASELINE, 18));
+        //userInputTextField.setLineWrap(true); only for JtextArea
 
-        //create userInputTextfield
+        //add instructionLabel to userInputPanel JPanel
+        userInputPanel.add(instructionLabel);
+        //add userInputTextArea to userInputPanel Jpanel
+        userInputPanel.add(userInputTextField);
 
 
-        //line 3
+        //format buttons
+        Font buttonFont = new Font(Font.SANS_SERIF, Font.ITALIC, 26);
+        //format check your sentence button
+        checkButton.setFont(buttonFont);
+        checkButton.setForeground(Color.BLUE);
+        checkButton.setAlignmentY(Component.CENTER_ALIGNMENT);
+        checkButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        checkButton.setMinimumSize(new Dimension(300,50));
+
+        //format exitGame button
+        exitButton.setFont(buttonFont);
+        exitButton.setForeground(Color.RED);
+        exitButton.setAlignmentY(Component.CENTER_ALIGNMENT);
+        exitButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        exitButton.setMinimumSize(new Dimension(300,50));
+
+        //add everything to main panel
+        //add(component); 
+
+        add(userInputPanel); //contains instructions and inputTextField
+        add(checkButton);
+        add(createBlankPanel(300, 20));
+        add(exitButton);
+
 
 
     }
     
-}
-
-import javax.swing.JPanel;
-
-public class LevelsPanel extends JPanel{
-
-    LevelsPanel(){
-        int score;
-        String[] nounVariables;
-        String[] verbVariables;
-        String[] subjectVariables;
-        
-        //set panel format (BoxLayout)
-        //set panel format( boxLayout)
-
-        
-        //line 1
-        //create & format score jpanel
-
-        //Create & format score jlabel
-
-        //create format scoretotal Label
-        
-        //add scoreJLabel & scoretotalLabel to scoreJPanel
-
-
-
-        //add scoreJLabel & scoretotalLabel to scoreJPanel
-        
-        
-
-        //line 2
-        //create titleLabel JLabel w/set Horizontal alignement = jlabel.center
-
-
-        //line 3-5
-        //create middlepanel, type JPanel
-
-        //create & format nounPanel
-
-        //for inside noun panel
-        //create &format nounLabel
-
-        //create & format nounTextfield
-
-        //add nounLabel and nounTextfield to nounPanel
-
-
-        //create userInputPanel
-
-        //inside userInputPanel:
-        //create instructionLabel
-
-        //create userInputTextfield
-
-
-        //line 3
-
-
+    //create blank panels method
+    JPanel createBlankPanel(int x, int y){
+        JPanel blankPanel = new JPanel();
+        //blankPanel.setSize(x,y);
+        blankPanel.setMaximumSize(new Dimension(x, y));
+        blankPanel.setPreferredSize(new Dimension(x, y));
+        //blankPanel.setBackground(Color.BLACK);
+        return blankPanel;
     }
-    
 }
- master
