76a2b6808165e06a56ac23ee6d81df6835b261ba
DanielaRL <102562736+DanielaRL@users.noreply.github.com>
2022-04-26 17:26:24 -0500

    test input/out streams

--- BuildBuddiesMainWindow.java
+++ BuildBuddiesMainWindow.java
@@ -2,6 +2,9 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
+
 
 public class BuildBuddiesMainWindow extends JFrame {
     static CardLayout cLayout;
@@ -42,6 +45,13 @@ public class BuildBuddiesMainWindow extends JFrame {
             public void actionPerformed(ActionEvent e){
                 //method for checkbutton
                 JOptionPane.showMessageDialog(null, "Computer comparing grammar file to user input.");
+                //method to check grammar
+                try {
+                    runGrammarCheck();
+                } catch (Exception e1) {
+                    // Auto-generated catch block
+                    e1.printStackTrace();
+                }
             }
         });
 
@@ -64,6 +74,36 @@ public class BuildBuddiesMainWindow extends JFrame {
         cLayout.show(panelContainer, "1"); //show card1 when new instance is created
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
+    }
+
+    void runGrammarCheck() throws Exception{
+        //get input
+        String test = "Cat ate food.";
+
+        //determine if user supplied a file for input, or if we should instead read command line input
+
+        String inputFile = test;
+        InputStream is =inputFile;
+        
+        //if(args.length > 0) inputFile = args[0];
+        
+        //InputStream is = System.in;     //create the input stream?
+
+        //if(inputFile != null) is = new FileInputStream(inputFile);
+        ANTLRInputStream input = new ANTLRInputStream(is);
+
+        
+        //creates the input stream and inserts a text file
+        //FileInputStream is = new FileInputStream("D:/Libraries/Documents/Spring 2022/CSEN 4366 Programing Languages/BuildingBuddies/test.txt")
+        FileInputStream is = new FileInputStream(test);
+        is.close(); //closes the input stream
+        //create lexer, 
+        SentenceGrammarLexer lexer = new SentenceGrammarLexer(input);
+
+        //create token buffer, 
+        
+        //create parser
+
     }
 
     public static void main(String[] args) {
