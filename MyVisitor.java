79041319ce559f9e14a8bb7c8ccd2d4c52886cbd
David-H21 <103387445+David-H21@users.noreply.github.com>
2022-05-03 00:02:44 -0500

    Update MyVisitor.java

--- MyVisitor.java
+++ MyVisitor.java
@@ -17,60 +17,32 @@ public class MyVisitor extends SentenceGrammarBaseVisitor<Object>{
     @Override
     public Object visitSimpleSentence(SentenceGrammarParser.SimpleSentenceContext ctx) {
         //  Auto-generated method stub
-        //JOptionPane.showMessageDialog(null, "Visited simpleSentence");
 
         String temp= LevelsPanel.userInputTextArea.getText();
-        //JOptionPane.showMessageDialog(null, "temp is "+temp);
-
-        //JOptionPane.showMessageDialog(null, "noun variable are "+LevelsPanel.nounVariables);
         
         for(int i = 0; i < LevelsPanel.nounVariables.length; i++){
-            //JOptionPane.showMessageDialog(null, "noun variable are "+LevelsPanel.nounVariables[i]);
                 if(temp.contains(LevelsPanel.nounVariables[i])){
-                    //JOptionPane.showMessageDialog(null, "noun variable are "+LevelsPanel.nounVariables[i]);
                     hasNoun=true;
-                    //JOptionPane.showMessageDialog(null, "noun is "+hasNoun);
                     break;
                 }
         }
 
         for(int i = 0; i < LevelsPanel.objectVariables.length; i++){
-            //JOptionPane.showMessageDialog(null, "noun variable are "+LevelsPanel.nounVariables[i]);
                 if(temp.contains(LevelsPanel.objectVariables[i])){
-                    //JOptionPane.showMessageDialog(null, "noun variable are "+LevelsPanel.nounVariables[i]);
                     hasObject=true;
-                    //JOptionPane.showMessageDialog(null, "object is "+hasObject);
                     break;
                 }
         }
 
         for(int i = 0; i < LevelsPanel.verbVariables.length; i++){
-            //JOptionPane.showMessageDialog(null, "noun variable are "+LevelsPanel.nounVariables[i]);
                 if(temp.contains(LevelsPanel.verbVariables[i])){
-                    //JOptionPane.showMessageDialog(null, "noun variable are "+LevelsPanel.nounVariables[i]);
                     hasVerb=true;
-                    //JOptionPane.showMessageDialog(null, "verb is "+hasVerb);
                     break;
                 }
         }
-
-        /*if (Arrays.asList(LevelsPanel.objectVariables).contains(temp)==true){
-            hasObject=true;
-            JOptionPane.showMessageDialog(null, "object is "+hasObject);
-        }else{
-            JOptionPane.showMessageDialog(null, "object is "+hasObject);
-        }
-        if (Arrays.asList(LevelsPanel.verbVariables).contains(temp)==true){
-            hasVerb=true;
-            JOptionPane.showMessageDialog(null, "verb is "+hasVerb);
-        }*/
         if(hasNoun && hasVerb && hasObject){
             BuildBuddiesMainWindow.simpleSentece=true;
-            //JOptionPane.showMessageDialog(null, "SimpleSentence "+BuildBuddiesMainWindow.simpleSentece);
 
-        }else{
-            //JOptionPane.showMessageDialog(null, "SimpleSentence "+BuildBuddiesMainWindow.simpleSentece);
-        }
         
         
         return super.visitSimpleSentence(ctx);
